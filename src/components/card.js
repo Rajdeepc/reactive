b1a10cda840d50d11d4308df6b51861a72197088
Rajdeepc <rajrock38@gmail.com>
2019-07-10 23:53:40 +0530

    new commit

--- src/components/card.js
+++ src/components/card.js
@@ -3,10 +3,10 @@ import { Link } from "gatsby";
 
 const Card = (props) => (
     <div>
-        <div style={{ paddingTop:'20px',paddingBottom:'20px',background: 'white', borderBottom:'1px solid rgba(0, 0, 0, 0.1)' }}>
-            <h3 style={{fontSize:'25.2px',marginBottom:0,lineHeight:'30px',fontWeight:900}}><a href="_blank" to={props.item.url}>{props.item.title}</a></h3>
-            <p style={{fontSize:'18.96px',color:'rgba(0, 0, 0, 0.54)',marginBottom:'10px',marginTop:'10px'}}>{props.item.description ? props.item.description : ''}</p>
-            <p style={{fontSize:'18.96px',color:'rgba(0,0,0,0.8)',marginBottom:0}}>Framework: {props.item.framework}</p>
+        <div style={{ paddingTop: '20px', paddingBottom: '20px', background: 'white', borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}>
+            <h3 style={{ fontSize: '25.2px', marginBottom: 0, lineHeight: '30px', fontWeight: 900 }}><a target="_blank" href={`https://${props.item.url}`}>{props.item.title}</a></h3>
+            <p style={{ fontSize: '18.96px', color: 'rgba(0, 0, 0, 0.54)', marginBottom: '10px', marginTop: '10px' }}>{props.item.description ? props.item.description : ''}</p>
+            <p style={{ fontSize: '18.96px', color: 'rgba(0,0,0,0.8)', marginBottom: 0 }}>Framework: {props.item.framework}</p>
             <small>Date Created: {props.item.created ? props.item.created : ''}</small>
         </div>
     </div>
