4d862a8d527d1cf27a10fb50b6556ccde4eccf8e
Rajdeepc <rajrock38@gmail.com>
2020-03-29 00:04:23 +0530

    removed about me

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,51 +1,18 @@
-// import React from "react"
-// import { Link, graphql } from "gatsby"
-// import CustomDate from '../components/customDate';
-// import Layout from "../components/layout"
-// import { calTimeRead } from '../components/timeToRead';
+import React from "react"
+import { Link, graphql } from "gatsby"
+import CustomDate from '../components/customDate';
+import Layout from "../components/layout"
+import { calTimeRead } from '../components/timeToRead';
 
-// const IndexPage = ({ data }) => (
-//     <Layout>
-//         <div className="text-right">{data.allMarkdownRemark.edges.length} blogs</div>
-//         {data.allMarkdownRemark.edges.map((post,i) => (
-//             <div key={post.node.id} >
-//             <div style={{ paddingTop:'20px',paddingBottom:'20px',background: 'white'}}>
-//                 <h3 style={{marginBottom:10,fontSize:'1.75rem'}}><Link 
-//                 style={{ fontWeight:900}}
-//                 to={
-//                     post.node.frontmatter.path
-//                 }>{post.node.frontmatter.title}</Link></h3>
-//                 <small>{ CustomDate(post.node.frontmatter.date)} &nbsp;&nbsp; <i className="fa fa-coffee" aria-hidden="true"/> {calTimeRead(post.node.html)}</small>
-                
-//             </div>
-//             { !(data.allMarkdownRemark.edges.length === i + 1) && <hr></hr> }
-//             </div>
-//         ))}
-//     </Layout>
-// )
-
-// export const pageQuery = graphql`
-//     query BlogIndexQuery 
-//         {
-// 	allMarkdownRemark {
-//     edges {
-//       node {
-//         id
-//         html
-//         frontmatter {
-//           path
-//           title
-//           date
-//         }
-//         excerpt
-//       }
-//     }
-  
-// }
-//     }
-// `
+const BlogPage = ({ data }) => (
+    <Layout>
+        
+    </Layout>
+)
 
 
-// export default IndexPage
+
+
+export default BlogPage
 
 
