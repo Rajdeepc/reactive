8c052b8fec99ba118fec05976c83ace25aec3e74
Rajdeepc <rajrock38@gmail.com>
2020-04-26 12:15:19 +0530

    experiements

--- src/pages/fiddles.js
+++ src/pages/fiddles.js
@@ -1,39 +1,39 @@
-// import React, { useEffect,useState } from "react";
+import React, { useEffect,useState } from "react";
 
-// import Layout from "../components/layout";
-// import Card from '../components/card';
-// import Loader from '../components/loader';
+import Layout from "../components/layout";
+import Card from '../components/card';
+import Loader from '../components/loader';
 
-// const FiddlesPage = () => {
+const FiddlesPage = () => {
 
-//   const[data,setData] = useState([])
+  const[data,setData] = useState([])
 
-//   useEffect(() => {
-//     fetch('http://jsfiddle.net/api/user/rajrock38/demo/list.json')
-//       .then(response => {
-//         return response.json()
-//       })
-//       .then(data => {
-//         setData(data);
-//       })
+  useEffect(() => {
+    fetch('http://jsfiddle.net/api/user/rajrock38/demo/list.json')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setData(data);
+      })
 
-//   }, [])
+  }, [])
 
-//   return (
-//     <Layout>
-//       <div className="text-right">{data.length} fiddles</div>
-//       { data.length > 0 ? data.map((item,i) => {
-//         return (
-//           <div>
-//             <div item={item} key={item.id}/>
-//             { (data.length === i + 1) && <hr></hr>}
-//         </div>
-//         )
-//       }): <Loader/>}
+  return (
+    <Layout>
+      <div className="text-right">{data.length} fiddles</div>
+      { data.length > 0 ? data.map((item,i) => {
+        return (
+          <div>
+            <div item={item} key={item.id}/>
+            { (data.length === i + 1) && <hr></hr>}
+        </div>
+        )
+      }): <Loader/>}
       
-//       </Layout>
-//   )
+      </Layout>
+  )
 
-// }
+}
 
-// export default FiddlesPage
+export default FiddlesPage
